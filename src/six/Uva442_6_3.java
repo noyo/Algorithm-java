9edbc84d8700706c87d3215f7526f7dd4422b3f1
1471551330@qq.com <1471551330@qq.com>
2018-04-04 23:35:16 +0800

    6-3 uva442

--- src/six/Uva442_6_3.java
+++ src/six/Uva442_6_3.java
@@ -5,6 +5,7 @@
 package six;
 
 import java.util.List;
+import java.util.Stack;
 
 import util.IOUtils;
 
@@ -34,10 +35,58 @@ public class Uva442_6_3 {
 		H = new Matrix(10, 20);
 		I = new Matrix(20, 25);
 	}
+	
+	private Matrix createMartix(char c) {
+		switch (c) {
+		case 'A':
+			return A;
+		case 'B':
+			return B;
+		case 'C':
+			return C;
+		case 'D':
+			return D;
+		case 'E':
+			return E;
+		case 'F':
+			return F;
+		case 'G':
+			return G;
+		case 'H':
+			return H;
+		case 'I':
+			return I;
+		}
+		return null;
+	}
 
+	private long executeCount(char[] s) {
+		Stack<Matrix> params = new Stack<>();
+		long product = 0;
+		for (char c : s) {
+			if (c == '(') {
+				continue;
+			}
+			if (c == ')') {
+				Matrix B = params.pop();
+				Matrix A = params.peek();
+				int muti = A.multi(B);
+				if (muti == -1) return muti;
+				product += muti;
+			} else {
+				params.push(createMartix(c));
+			}
+		}
+		return product;
+	}
+	
 	public static void main(String args[]) {
 		
+		Uva442_6_3 uva = new Uva442_6_3();
 		
+		for (String s : uva.data) {
+			System.out.println(uva.executeCount(s.toCharArray()));
+		}
 		
 	}
 	
@@ -52,5 +101,20 @@ class Matrix {
 		public Matrix(int n, int m) {
 			this.n = n;
 			this.m = m;
+		}
+		
+		/**
+		 * 矩阵乘法
+		 * 
+		 * @param A
+		 * @param B
+		 * @return
+		 */
+		public int multi(Matrix B) {
+			if (null == B) throw new IllegalArgumentException("the Matrix can't be null");
+			if (m != B.n) {
+				return -1;
+			}
+			return n * m * B.n;
 		}
 	}
