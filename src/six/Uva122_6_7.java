f2f43a39e03333e15b5979d4e5deb23aa6bde676
Chris <1471551330@qq.com>
2018-04-09 15:51:38 +0800

    uva 122 6-7

---
+++ src/six/Uva122_6_7.java
@@ -0,0 +1,86 @@
+package six;
+
+import util.IOUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Copyright © 2018 Chris. All rights reserved.
+ * <p>
+ * Description:
+ * <p>
+ * Package: six
+ * author: Chris
+ * date: 2018/4/9 14:59
+ */
+public class Uva122_6_7 {
+
+    private List<String> data = IOUtils.readFromFile("6/7/in.txt");
+    private Node root;
+
+    private void layerDisplay(Node bTree) {
+        if (null == bTree) return;
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.push(bTree);
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            System.out.print(node.val + " ");
+            if (null != node.left) {
+                queue.addLast(node.left);
+            }
+            if (null != node.right) {
+                queue.addLast(node.right);
+            }
+        }
+        System.out.println();
+        root = null;
+    }
+
+    private void insertNode(int val, String route) {
+        if (null == root) root = new Node();
+        Node node = root;
+        int size = route.length();
+        for (int i = 0; i < size; i++) {
+            if (route.charAt(i) == 'L') {
+                node = node.getLeft();
+            } else {
+                node = node.getRight();
+            }
+        }
+        node.val = node.valued ? -1 : val;
+        node.valued = true;
+    }
+
+    public static void main(String args[]) {
+
+        Uva122_6_7 uva = new Uva122_6_7();
+        for (String s : uva.data) {
+            String ss[] = s.split(",");
+            uva.insertNode(Integer.parseInt(ss[0].substring(1)), ss[1].substring(0, ss[1].length() - 1));
+        }
+        uva.layerDisplay(uva.root);
+
+    }
+
+    class Node {
+        int val;
+        boolean valued = false;
+        Node left;
+        Node right;
+
+        Node getLeft() {
+            if (null == left) {
+                left = new Node();
+            }
+            return left;
+        }
+
+        Node getRight() {
+            if (null == right) {
+                right = new Node();
+            }
+            return right;
+        }
+    }
+}
